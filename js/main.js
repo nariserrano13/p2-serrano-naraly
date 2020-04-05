a0ed865503d8741f8d32100b69a858e6619f3600
Naraly Serrano <naralycserrano@ufl.edu>
2020-04-05 09:29:02 -0400

    Add tabbed js script for programs page

--- js/main.js
+++ js/main.js
@@ -39,7 +39,8 @@ function involved_partner(){
 //       fundraiseIcon.classList.toggle('involved-collapse-content');
 // }
 
-
+/*CODE FOR API 
+*/
 //http://worldtimeapi.org/api/timezone/America/New_York.txt.
 
 var xmlhttp = new XMLHttpRequest();
@@ -52,12 +53,13 @@ xmlhttp.onreadystatechange = function() {
 
         //Code Dealing With The API Data Goes Here
 
-          //this console.log(apiResult.datetime);
-          //gave 2020-04-04T15:07:41.852317-04:00 in console
+          //this by itself " console.log(apiResult.datetime); ""
+          //gave 2020-04-04T15:07:41.852317-04:00 as a string 
+          //in console & needed to be sliced using .slice
         console.log(apiResult.datetime);
 
         var month = apiResult.datetime.slice(5,7);
-        console.log(month);
+        console.log(month); 
 
         var day = apiResult.datetime.slice(8,10);
         console.log(day);
@@ -69,3 +71,45 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open('GET', 'http://worldtimeapi.org/api/timezone/America/New_York', true);
 xmlhttp.send();
 
+ //CODE FOR TABBED CONTENT
+
+ function clubsTab(){
+  var programsContent = document.getElementsByClassName("programs-content");
+
+    programsContent[1].style.display="none";
+    programsContent[2].style.display="none";
+  
+    var tabButton = document.getElementsByClassName("tabButton");
+    tabButton[1].className = tabButton[1].className.replace("active", " ");
+    tabButton[2].className = tabButton[2].className.replace("active", " ")
+
+    programsContent[0].style.display="block";
+
+}
+function summerTab(){
+  var programsContent = document.getElementsByClassName("programs-content");
+  
+  programsContent[0].style.display="none";
+  programsContent[2].style.display="none";
+
+  var clubContent=document.getElementById("clubs");
+  clubContent.style.display="none"
+
+  var tabButton = document.getElementsByClassName("tabButton");
+  tabButton[0].className = tabButton[0].className.replace("active", " ");
+  tabButton[2].className = tabButton[2].className.replace("active", " ")
+
+  programsContent[1].style.display="block";
+}
+function collegeTab(){
+  var programsContent = document.getElementsByClassName("programs-content");
+
+  programsContent[0].style.display="none";
+  programsContent[1].style.display="none";
+
+  var tabButton = document.getElementsByClassName("tabButton");
+  tabButton[0].className = tabButton[0].className.replace("active", " ");
+  tabButton[1].className = tabButton[1].className.replace("active", " ")
+
+  programsContent[2].style.display="block";
+}
